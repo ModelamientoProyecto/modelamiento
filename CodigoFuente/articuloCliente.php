66f6761b8acd3a573fd891a65b7205d253d79d77
ModelamientoProyecto <karin.joss@hotmail.com>
2018-05-22 10:16:21 -0500

    avance
    avanc

---
+++ CodigoFuente/articuloCliente.php
@@ -0,0 +1,70 @@
+<?php
+//Activamos el almacenamiento en el buffer
+ob_start();
+session_start();
+
+if (!isset($_SESSION["nombre"]))
+{
+  header("Location: login.html");
+}
+else
+{
+// require 'headerCliente.php';
+  require 'headerCliente.php';
+if ($_SESSION['almacen']==1)
+{
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Artículos</h1>
+                        <div class="box-tools pull-right">
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body table-responsive" id="listadoregistros">
+                        <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover" align="center">
+                          <thead>
+                            <th>Categoría</th>
+                            <th>Imagen</th>
+                          </thead>
+                          <tbody>                            
+                          </tbody>
+                          <tfoot>
+                            <th>Categoría</th>
+                            <th>Imagen</th>
+                          </tfoot>
+                        </table>
+                    </div>
+
+                    </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+<?php
+}
+else
+{
+  require 'noacceso.php';
+}
+require 'footerCliente.php';
+?>
+<script type="text/javascript" src="../public/js/JsBarcode.all.min.js"></script>
+<script type="text/javascript" src="../public/js/jquery.PrintArea.js"></script>
+<script type="text/javascript" src="scripts/articuloCliente.js"></script>
+<?php 
+}
+ob_end_flush();
+?>
